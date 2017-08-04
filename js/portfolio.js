ec312499e55d44d236a8fd87cc3686eabcd1ca30
beebul <beebul@gmail.com>
2017-08-04 14:16:52 +0930

    check in

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,28 +1,28 @@
-// // init Isotope
-// var $grid = $('.grid').isotope({
-//   // options
-// });
-// // filter items on button click
-// $('.filter-button-group').on('click', 'a', function() {
-//   var filterValue = $(this).attr('data-filter');
-//   $grid.isotope({
-//     filter: filterValue
-//   });
-// });
-// $('.button-group a.button').on('click', function() {
-//   $('.button-group a.button').removeClass('active');
-//   $(this).addClass('active');
-// });
-// 
-// 
-// $('div.modal').on('show.bs.modal', function() {
-//   var modal = this;
-//   var hash = modal.id;
-//   console.log(this);
-//   window.location.hash = hash;
-//   window.onhashchange = function() {
-//     if (!location.hash) {
-//       $(modal).modal('hide');
-//     }
-//   }
-// });
+// init Isotope
+var $grid = $('.grid').isotope({
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on('click', 'a', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({
+    filter: filterValue
+  });
+});
+$('.button-group a.button').on('click', function() {
+  $('.button-group a.button').removeClass('active');
+  $(this).addClass('active');
+});
+
+
+$('div.modal').on('show.bs.modal', function() {
+  var modal = this;
+  var hash = modal.id;
+  console.log(this);
+  window.location.hash = hash;
+  window.onhashchange = function() {
+    if (!location.hash) {
+      $(modal).modal('hide');
+    }
+  }
+});
