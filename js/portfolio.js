554c96975bdd3b7c4b3cb7ca18c967bbbc9d698f
beebul <beebul@gmail.com>
2017-08-09 08:48:31 +0930

    Stuff

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,28 +1,68 @@
-// init Isotope
-var $grid = $('.grid').isotope({
-  // options
-});
-// filter items on button click
-$('.filter-button-group').on('click', 'a', function() {
-  var filterValue = $(this).attr('data-filter');
-  $grid.isotope({
-    filter: filterValue
+$(window).load(function() {
+  // init Isotope
+  var $grid = $('.grid').isotope({
+    // options
   });
-});
-$('.button-group a.button').on('click', function() {
-  $('.button-group a.button').removeClass('active');
-  $(this).addClass('active');
-});
+  // filter items on button click
+  $('.filter-button-group').on('click', 'a', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({
+      filter: filterValue
+    });
+  });
+  $('.button-group a.button').on('click', function() {
+    $('.button-group a.button').removeClass('active');
+    $(this).addClass('active');
+  });
 
 
-$('div.modal').on('show.bs.modal', function() {
-  var modal = this;
-  var hash = modal.id;
-  console.log(this);
-  window.location.hash = hash;
-  window.onhashchange = function() {
-    if (!location.hash) {
-      $(modal).modal('hide');
+  $(function() {
+    $('.easy-modal').easyModal({
+      top: 200,
+      overlay: 0.2
+    });
+  
+    $('.easy-modal-open').click(function(e) {
+      var target = $(this).attr('href');
+      $(target).trigger('openModal');
+      e.preventDefault();
+      
+      
+      var modal = this;
+      var hash = modal.id;
+      window.location.hash = hash;
+      window.onhashchange = function() {
+        if (!location.hash) {
+          $(modal).modal('show');
+        }
+      }
+      
+    });
+  
+    $('.easy-modal-close').click(function(e) {
+      $('.easy-modal').trigger('closeModal');
+    });
+  
+    $('.easy-modal-animated').easyModal({
+      top: 200,
+      overlay: 0.2,
+      transitionIn: 'animated bounceInLeft',
+      transitionOut: 'animated bounceOutRight',
+      closeButtonClass: '.animated-close'
+    });
+  });
+
+  $('.easy-modal-open').on('show.bs.modal', function() {
+    var modal = this;
+    var hash = modal.id;
+    console.log(this);
+    window.location.hash = hash;
+    window.onhashchange = function() {
+      if (!location.hash) {
+        $(modal).modal('hide');
+      }
     }
-  }
+  });
+
+
 });
