b3e4941c57778dba1201ab8b51d4e3ed90a57123
beebul <beebul@gmail.com>
2017-08-09 15:16:28 +0930

    Kind of got modal working

--- js/portfolio.js
+++ js/portfolio.js
@@ -21,28 +21,18 @@ $(window).load(function() {
       top: 200,
       overlay: 0.2
     });
-  
+
     $('.easy-modal-open').click(function(e) {
       var target = $(this).attr('href');
+      console.log(this);
       $(target).trigger('openModal');
       e.preventDefault();
-      
-      
-      var modal = this;
-      var hash = modal.id;
-      window.location.hash = hash;
-      window.onhashchange = function() {
-        if (!location.hash) {
-          $(modal).modal('show');
-        }
-      }
-      
     });
-  
+
     $('.easy-modal-close').click(function(e) {
       $('.easy-modal').trigger('closeModal');
     });
-  
+
     $('.easy-modal-animated').easyModal({
       top: 200,
       overlay: 0.2,
@@ -50,18 +40,6 @@ $(window).load(function() {
       transitionOut: 'animated bounceOutRight',
       closeButtonClass: '.animated-close'
     });
-  });
-
-  $('.easy-modal-open').on('show.bs.modal', function() {
-    var modal = this;
-    var hash = modal.id;
-    console.log(this);
-    window.location.hash = hash;
-    window.onhashchange = function() {
-      if (!location.hash) {
-        $(modal).modal('hide');
-      }
-    }
   });
 
 
