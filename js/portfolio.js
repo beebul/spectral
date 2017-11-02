2de1abbb1986096bfd8687e5c9268ed9b9848459
David McGarry <beebul@gmail.com>
2017-11-02 11:10:50 +1030

    lazyload and resized

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,61 +1,55 @@
 $(document).ready(function() {
-    let images = document.querySelectorAll(".large-portfolio-image");
-    new LazyLoad(images);
-});
 
-
-$(window).load(function() {
-  // init Isotope
-  var $grid = $('.grid').isotope({
-    // options
-  });
-  // filter items on button click
-  $('.filter-button-group').on('click', 'a', function() {
-    var filterValue = $(this).attr('data-filter');
-    $grid.isotope({
-      filter: filterValue
+    // init Isotope
+    var $grid = $('.grid').isotope({
+        // options
     });
-  });
-  $('.button-group a.button').on('click', function() {
-    $('.button-group a.button').removeClass('active');    
-    $(this).addClass('active');
-  });
-
-
-  $(function() {
-    
-    $('.easy-modal').easyModal({
-      top: 1, 
-      overlay: 0.2,
-      onOpen: function(modal) { 
-        $(modal).addClass('modal-open');
-        //$(modal).children('img').each(function(img) { img.attr('src', img.attr('data-src')); });
-      }, 
-      onClose: function(modal) 
-      { 
-        $(modal).removeClass('modal-open'); 
-      }
+    // filter items on button click
+    $('.filter-button-group').on('click', 'a', function() {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: filterValue
+        });
+    });
+    $('.button-group a.button').on('click', function() {
+        $('.button-group a.button').removeClass('active');
+        $(this).addClass('active');
     });
 
-    $('.easy-modal-open').click(function(e) {
-      var target = $(this).attr('href');
-      $(target).trigger('openModal');
-      e.preventDefault();
+
+    $(function() {
+
+        $('.easy-modal').easyModal({
+            top: 1,
+            overlay: 0.2,
+            onOpen: function(modal) {
+                $(modal).addClass('modal-open');
+                //$(modal).children('img').each(function(img) { img.attr('src', img.attr('data-src')); });
+            },
+            onClose: function(modal) {
+                $(modal).removeClass('modal-open');
+            }
+        });
+
+        $('.easy-modal-open').click(function(e) {
+            var target = $(this).attr('href');
+            $(target).trigger('openModal');
+            e.preventDefault();
+        });
+
+        $('.easy-modal-close').click(function(e) {
+            $('.easy-modal').trigger('closeModal');
+        });
+
+        $('.easy-modal-animated').easyModal({
+            top: 200,
+            overlay: 0.2,
+            transitionIn: 'animated bounceInLeft',
+            transitionOut: 'animated bounceOutRight',
+            closeButtonClass: '.animated-close'
+        });
+
     });
-
-    $('.easy-modal-close').click(function(e) {
-      $('.easy-modal').trigger('closeModal');
-    });
-
-      $('.easy-modal-animated').easyModal({
-    top: 200,
-    overlay: 0.2,
-    transitionIn: 'animated bounceInLeft',
-    transitionOut: 'animated bounceOutRight',
-    closeButtonClass: '.animated-close'
-  });
-
-  });
 
 
 });
